942bfc84d7459d053fbbd8231e0176c6d3205446
Junaid Hassan <junaidhassan225588@gmail.com>
2025-01-31 14:53:52 +0500

    5:40 - completed product detail page

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Product;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', [ProductController::class, 'home'])->name('dashboard');
 Route::get('/show/{product:slug}', [ProductController::class, 'show'])->name('product.show');
+Route::post('/cart/store/{product}', function (Product $product) {
 
+})->name('cart.store');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
